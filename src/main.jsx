bfb978663dfb41e46142be0dd9baae22b8addb06
unknown <ttra170302@gmail.com>
2022-03-07 10:59:43 +0700

    init project

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const person = {
+  name:"tra",
+  age: 20
+}
+
+ReactDOM.render(<div>
+<p>{person.name}</p>
+<p>{person.age}</p>
+
+</div>,document.getElementById('root')
 )
