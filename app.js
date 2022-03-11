fb097b2867573ca1e44f3e7410600d2dcba21e72
unknown <ttra170302@gmail.com>
2022-03-11 09:12:04 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var myName = "Thanh Tra";
+var age = 20;
+var myStatus = true;
+var myObj = { id: 1, name: "Tra" };
+var arrNumber = [1, 2, 3, 4];
+var String = ["a", "b", "c", "d"];
+var arrObj = [{ id: 3, name: "san pham a" }];
+function sum(numA, numB) {
+    return numA + numB;
+}
