39e908882869af3d460ca2c51ab91881d23642c5
unknown <ttra170302@gmail.com>
2022-03-15 23:22:22 +0700

    app.ts

---
+++ app.ts
@@ -0,0 +1,17 @@
+export{};
+type Product = {
+   id:number ,
+   name:string,
+}
+
+
+const myName: string = "Thanh Tra";
+const age:number = 20;
+const myStatus:boolean = true;
+const myObj: Product = {id:1, name:"Tra"};
+const arrNumber: number[]=[1,2,3,4];
+const String: string[]=["a","b","c","d"]
+const arrObj: Product[]=[{id:3,name:"san pham a"}]
+function sum(numA,numB){
+    return numA + numB;
+}
